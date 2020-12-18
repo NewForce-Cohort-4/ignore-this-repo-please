b184e1ff9f767e7bc59faa62fa91950e109c6495
Jordan Castelloe <jordan.castelloe@gmail.com>
2020-12-17 20:07:41 -0500

    added comments

--- script.js
+++ script.js
@@ -34,9 +34,13 @@ let partyGuests = [
 ]
 
 
+// Select the div where we want to print if they CAN drink
 let over21Container = document.querySelector(".over-21-container")
+
+// select the div where we want to print if they CAN'T drink
 let under21Container = document.querySelector(".under-21-container")
 
+// This function will accept a person object and spit out an HTML representation of that person object
 function printPartyGuest(personObject){
     return `<div class="party-guest-card">
         <h3>${personObject.name}</h3>
@@ -45,10 +49,14 @@ function printPartyGuest(personObject){
 }
 
 
+// loop through all the party guests
 for(let i = 0; i < partyGuests.length; i++){
+    // For each guest, check if they're over 21
     if(partyGuests[i].age >= 21){
+        // if they are, print them to the over 21 container
         over21Container.innerHTML += printPartyGuest(partyGuests[i])
     } else {
+        // if not, print them to the under 21 container
         under21Container.innerHTML += printPartyGuest(partyGuests[i])
     }
 }
